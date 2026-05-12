002a8e357592809e27eda07bc2ab3872efc9a52a
Nia.Ngosi <nia.ngosi@digital.homeoffice.gov.uk>
2026-05-12 15:14:51 +0100

    CCL-8850 - Adjusting create iam role referencing

--- main.tf
+++ main.tf
@@ -1,35 +1,31 @@
-data "aws_iam_policy_document" "pod_identity_assume" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["pod.eks.amazon.com"]
-    }
-
-    actions = [
-      "sts.AssumeRole",
-      "sts:TagSession"
-    ]
-  }
-}
-
 resource "aws_iam_role" "this" {
-  count              = var.create_role != false ? 1 : 0
-  name               = var.role_name
-  assume_role_policy = data.aws_iam_policy_document.pod_identity_assume.json
-
+  count = var.create_role == false ? 1 : 0
+  name  = var.role_name
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [{
+      Actions = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+      Effect = "Allow"
+      Principle = {
+        Service = "pod.eks.amazon.com"
+      }
+    }]
+  })
   tags = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "role_policy" {
   policy_arn = var.policy_arn
-  role       = coalesce(var.existing_role_name, aws_iam_role.this[0].name)
+  role = coalesce(var.existing_role_name,
+  try(aws_iam_role.this[0].name, null))
 }
 
 resource "aws_eks_pod_identity_association" "pod_identity_association" {
   cluster_name    = var.cluster_name
   namespace       = var.namespace
   service_account = var.service_account_name
-  role_arn        = coalesce(var.existing_role_arn, aws_iam_role.this[0].arn)
+  role_arn        = coalesce(var.existing_role_arn, try(aws_iam_role.this[0].arn, null))
 }
