c34075ee5e4c2c7bbd0fb74d62fe9fed3174c1f3
Nia.Ngosi <nia.ngosi@digital.homeoffice.gov.uk>
2026-05-12 15:26:34 +0100

    CCL-8850 - Adjusting create iam role referencing

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "this" {
-  count              = var.create_role == false ? 1 : 0
+  count              = var.create_role == true ? 1 : 0
   name               = var.role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   tags               = var.tags
