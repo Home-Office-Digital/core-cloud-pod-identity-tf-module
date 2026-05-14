0210fea0ef73ac678ff612f21ada52127018475e
Nia.Ngosi <nia.ngosi@digital.homeoffice.gov.uk>
2026-05-14 11:53:07 +0100

    feat: Adding pod identity module files

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "create_role" {
+  type    = bool
+  default = false
+}
+
+
+variable "tags" {
+  description = "Tags to be applied"
+  type        = map(string)
+  default     = {}
+
+  validation {
+    condition = alltrue([
+      contains(keys(var.tags), "account-code"),
+      contains(keys(var.tags), "cost-centre"),
+      contains(keys(var.tags), "portfolio-id"),
+      contains(keys(var.tags), "project-id"),
+      contains(keys(var.tags), "service-id"),
+      contains(keys(var.tags), "environment-type"),
+      contains(keys(var.tags), "owner-business"),
+      contains(keys(var.tags), "budget-holder"),
+      contains(keys(var.tags), "source-repo")
+    ])
+    error_message = "Tags must include all mandatory fields."
+  }
+}
+
+variable "existing_role_arn" {
+  type    = string
+  default = null
+}
+
+variable "role_name" {
+  type    = string
+  default = null
+}
+
+variable "existing_role_name" {
+  type    = string
+  default = null
+}
+
+variable "policy_arn" {
+  type    = string
+  default = null
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "service_account_name" {
+  type = string
+}
