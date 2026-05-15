2e1d983948e83ef84b5d8ad262cce4ad7f3d3363
Nia.Ngosi <nia.ngosi@digital.homeoffice.gov.uk>
2026-05-15 13:22:26 +0100

    feat: Testing IAM Role create pod identity module

--- main.tf
+++ main.tf
@@ -23,12 +23,12 @@ resource "aws_iam_role" "this" {
 
 resource "aws_iam_role_policy_attachment" "role_policy" {
   policy_arn = var.policy_arn
-  role       = try(aws_iam_role.this[0].name, null)
+  role       = try(aws_iam_role.this[1].name, null)
 }
 
 resource "aws_eks_pod_identity_association" "pod_identity_association" {
   cluster_name    = var.cluster_name
   namespace       = var.namespace
   service_account = var.service_account_name
-  role_arn        = coalesce(var.existing_role_arn, try(aws_iam_role.this[0].arn, null))
+  role_arn        = coalesce(var.existing_role_arn, try(aws_iam_role.this[1].arn, null))
 }
